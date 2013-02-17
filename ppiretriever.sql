e2f72c348dcb8b5013272f7c13d30b7ae555eadc
keuv <keuv@keuv-305U1A>
2013-02-17 18:20:52 +0100

    sql script: - basic table scheme - insert some known values

--- ppiretriever.sql
+++ ppiretriever.sql
@@ -8,7 +8,6 @@ drop table interaction_data cascade;
 drop table source_database cascade;
 drop table publication cascade;
 
-
 -- database creation
 create table if not exists protein (
 	id serial primary key,
