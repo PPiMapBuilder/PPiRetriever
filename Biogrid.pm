12cb10dc40b3413c233141ddf73144512e5c5b8e
pidupuis <pmj.dupuis@gmail.com>
2013-02-17 15:11:17 +0100

    Update Biogrid.pm
    Add comments

--- Biogrid.pm
+++ Biogrid.pm
@@ -24,7 +24,7 @@ sub parse {
 
 	my ($this) = @_;
 
-	my %hash_orga_tax = (
+	my %hash_orga_tax = ( # Hash to easily retrieve the correspondance between the taxonomy id and the seven reference organisms
 		'3702'  => 'Arabidopsis thaliana',
 		'6239'  => 'Caenorhabditis elegans',
 		'7227'  => 'Drosophilia Melanogaster',
@@ -34,9 +34,10 @@ sub parse {
 		'4896'  => 'Schizosaccharomyces pombe'
 	);
 
-	my $hash_orga_tax = {};
-	open( gene_name_to_uniprot_file, "gene_name_to_uniprot_database.txt" );
-	while (<gene_name_to_uniprot_file>) {
+	my $hash_orga_tax = {}; # A hash to store the uniprot id corresponding to a gene name and an organism
+				# This avoid to run the same request several times in the uniprot.org server
+	open( gene_name_to_uniprot_file, "gene_name_to_uniprot_database.txt" );  # A file to keep this hash
+	while (<gene_name_to_uniprot_file>) { # We initialize the hash with the data contained in the file
 		chomp($_);
 		my @convertion_data = split( /\t/, $_ );
 		$hash_orga_tax->{ $convertion_data[0] }->{ $convertion_data[2] } =
@@ -44,12 +45,12 @@ sub parse {
 	}
 	close(gene_name_to_uniprot_file);
 
-	open( data_file, "BIOGRID.txt" );
-	my $database = 'Biogrid';
+	open( data_file, "BIOGRID.txt" ); # We open the database file
+	my $database = 'Biogrid'; # We note the corresponding database we are using
 
 	my $i = 1;
 
-	open( gene_name_to_uniprot_file, ">>gene_name_to_uniprot_database.txt" );
+	open( gene_name_to_uniprot_file, ">>gene_name_to_uniprot_database.txt" ); # During this time, we complete the file which contains the uniprot id for a gene name and an organism
 	while (<data_file>) {
 
 		chomp($_);
@@ -73,17 +74,16 @@ sub parse {
 
 		my $orga_query;
 
-		my @data = split( /\t/, $_ );
+		my @data = split( /\t/, $_ ); # We split the line into an array
 
 		foreach my $tax_id ( keys %hash_orga_tax ) {
-			if ( $data[16] eq $tax_id ) {    # If the taxonomy id is present
-				$origin =
-				  $hash_orga_tax{$tax_id};    # We retrieve the organism name
+			if ( $data[16] eq $tax_id ) { # If the taxonomy id is present
+				$origin = $hash_orga_tax{$tax_id}; # We put the organism name into the "origin" variable
 				$orga_query = "$hash_orga_tax{$tax_id} [$tax_id]";
-				if ( $tax_id eq '9606' ) {
+				if ( $tax_id eq '9606' ) { #  If the organism is Homo sapiens, the "predictive from" will be "Human"
 					$pred = 'Human';
 				}
-				else {
+				else { # Else it will be "Interolog"
 					$pred = 'Interolog';
 				}
 				last;
@@ -91,23 +91,24 @@ sub parse {
 
 		}
 
-		if ( !$origin ) {
+		if ( !$origin ) { # If the origin is null, so if the interaction is not from one of the seven organisms, we do not consider this interaction
 			next;
 		}
 
-		my $internet = undef;
+		#my $internet = undef; # Temporary variable to see the number of request to the uniprot.org server
 
-		$intA = $data[7];
-		if ( exists( $hash_orga_tax->{$intA}->{$orga_query} ) ) {
-			$uniprot_A = $hash_orga_tax->{$intA}->{$orga_query};
+		$intA = $data[7]; # We retrieve the first interactor
+		if ( exists( $hash_orga_tax->{$intA}->{$orga_query} ) ) { # If the uniprot id has already been retrieved (and is now stored in the file)
+			$uniprot_A = $hash_orga_tax->{$intA}->{$orga_query}; # we retrieve it from the file
 		}
-		else {
-			$uniprot_A = gene_name_to_uniprot_id( $intA, $orga_query );
-			$hash_orga_tax->{$intA}->{$orga_query} = $uniprot_A;
-			print gene_name_to_uniprot_file "$intA\t$uniprot_A\t$orga_query\n";
-			$internet .= 'i';
+		else { # If we need to retrieve it from the web
+			$uniprot_A = gene_name_to_uniprot_id( $intA, $orga_query ); # We call the corresponding function
+			$hash_orga_tax->{$intA}->{$orga_query} = $uniprot_A; # We store it in the hash
+			print gene_name_to_uniprot_file "$intA\t$uniprot_A\t$orga_query\n"; # We store it in the file
+			#$internet .= 'i'; # We indicate that we used an internet connection
 		}
 
+		# Same principle as above
 		$intB = $data[8];
 		if ( exists( $hash_orga_tax->{$intB}->{$orga_query} ) ) {
 			$uniprot_B = $hash_orga_tax->{$intB}->{$orga_query};
@@ -116,27 +117,28 @@ sub parse {
 			$uniprot_B = gene_name_to_uniprot_id( $intB, $orga_query );
 			$hash_orga_tax->{$intB}->{$orga_query} = $uniprot_B;
 			print gene_name_to_uniprot_file "$intB\t$uniprot_B\t$orga_query\n";
-			$internet .= 'i';
+			# $internet .= 'i';
 		}
 
-		if ( !defined($uniprot_A) || !defined($uniprot_B) ) {
+		if ( !defined($uniprot_A) || !defined($uniprot_B) ) { # If the uniprot id was not retrieved, we do not keep the interaction
 			next;
 		}
 
-		$exp_syst = $data[11];
-		$pubmed   = $data[14] . "[uid]";
+		$exp_syst = $data[11]; # We retrieve the experimental system
+		$pubmed   = $data[14] . "[uid]"; # We retrieve the pubmed id
 
+		# Construction of the interaction elements
 		my @A = ( $uniprot_A, $intA );
 		my @B = ( $uniprot_B, $intB );
 		my @pubmed  = ($pubmed);
 		my @sys_exp = ($exp_syst);
 
-		my $interaction =
-		  Interaction->new( \@A, \@B, $origin, $database, \@pubmed, \@sys_exp );
+		# Construction of the interaction object
+		my $interaction = Interaction->new( \@A, \@B, $origin, $database, \@pubmed, \@sys_exp );
 
 		$this->SUPER::addInteraction($interaction);
 
-#print "$i $internet\t$intA\t$uniprot_A\t$intB\t$uniprot_B\t$exp_syst\t$origin\t$database\t$pubmed\t$pred\n";
+		#print "$i $internet\t$intA\t$uniprot_A\t$intB\t$uniprot_B\t$exp_syst\t$origin\t$database\t$pubmed\t$pred\n"; # Input for debug
 
 		$i++;
 
