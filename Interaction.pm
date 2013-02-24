ccab20bac4612a2b83482d25f9bfdc7e562170e5
keuv <keuv@keuv-305U1A>
2013-02-24 20:11:08 +0100

    Improvement of toString()

--- Interaction.pm
+++ Interaction.pm
@@ -1,61 +1,80 @@
 package Interaction;    # Nom du package, de notre classe
-use warnings;        # Avertissement des messages d'erreurs
-use strict;          # Vérification des déclarations
-use Carp;            # Utile pour émettre certains avertissements
-
+use warnings;           # Avertissement des messages d'erreurs
+use strict;             # Vérification des déclarations
+use Carp;               # Utile pour émettre certains avertissements
 
 #constructeur
 sub new {
- my ( $classe, $A, $B, $organism, $database, $pubmed, $sys_exp) = @_;    #on passe les données au constructeur
- my $this = {
-  "A" => [],
-  "B" => [],
-  "organism" => $organism,
-  "database" => $database,
-  "pubmed" => [],
-  "sys_exp" => []
- };
- 
- @{$this->{A}} = @{$A};
- @{$this->{B}} = @{$B};
- @{$this->{pubmed}} = @{$pubmed}; 
- @{$this->{sys_exp}} = @{$sys_exp};
- 
- bless( $this, $classe );      #lie la référence à la classe
+	my ( $classe, $A, $B, $organism, $database, $pubmed, $sys_exp ) =
+	  @_;               #on passe les données au constructeur
+	my $this = {
+		"A"        => [],
+		"B"        => [],
+		"organism" => $organism,
+		"database" => $database,
+		"pubmed"   => [],
+		"sys_exp"  => []
+	};
 
- return $this;		   #on retourne la référence consacrée
+	@{ $this->{A} }       = @{$A};
+	@{ $this->{B} }       = @{$B};
+	@{ $this->{pubmed} }  = @{$pubmed};
+	@{ $this->{sys_exp} } = @{$sys_exp};
+
+	bless( $this, $classe );    #lie la référence à la classe
+
+	return $this;               #on retourne la référence consacrée
 }
 
 sub afficheA {
 	my ($this) = @_;
-	foreach my $sub (@{$this->{A}}) {
-		print $sub."\t";
+	foreach my $sub ( @{ $this->{A} } ) {
+		print $sub. "\t";
 	}
 	print "\n";
 }
 
-
 sub organism {
-	my ( $this ) = @_;
+	my ($this) = @_;
 	return $this->{organism};
 }
 
 sub toString {
-	my ( $this ) = @_;
-	print "Interaction A : ".@{$this->{A}}[0]."  ".@{$this->{A}}[1]."\n";
-	print "Interaction B : ".@{$this->{B}}[0]."  ".@{$this->{B}}[1]."\n";
-	print "Organism : ".$this->{organism}."\n";
-	print "Database : ".$this->{database}."\n";
+	my ($this) = @_;
+	print "Interactor A:\n"
+	  . " - UniprotID: "
+	  . @{ $this->{A} }[0] . "\n"
+	  . " - Gene name:"
+	  . @{ $this->{A} }[1] . "\n"
+	  . print "Interactor B:\n"
+	  . " - UniprotID: "
+	  . @{ $this->{B} }[1] . "\n"
+	  . " - Gene name:"
+	  . @{ $this->{B} }[1] . "\n"
+	  . print "Organism : "
+	  . $this->{organism} . "\n";
+	print "Database : " . $this->{database} . "\n";
 	print "Pubmed : ";
-	foreach (sort @{$this->{pubmed}}) {print "$_\t";}
+	print " - $_\n" foreach ( @{ $this->{pubmed} } );
 	print "\nExperimental system : ";
-	foreach (sort @{$this->{sys_exp}}) {print "$_\t";}
-	print "\n";
+	print " - $_\n" foreach ( @{ $this->{sys_exp} } );
 }
-	
 
-1;    #Attention ! Obligatoire lors de la création d'un module !
-
+1;
 
 __END__
-Le compilateur ne lira pas les lignes après elle
+
+Format du toString() :
+	Interactor A:
+	 - UniprotID: uniprot1
+	 - Gene name: genename1
+	Interactor B:
+	 - UniprotID: uniprot2
+	 - Gene name: genename2
+	Organism: 9606
+	Database: biogrid
+	Pubmed:
+	 - 123456
+	 - 654321 
+	Experimental system:
+	 - two-hybrid 
