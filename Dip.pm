7951c1d397b4f9cd1b5c153894f802ba9c3a32ba
Piotr <pierre.cressant@gmail.com>
2013-03-09 23:42:36 +0100

    Minor changes

--- Dip.pm
+++ Dip.pm
@@ -45,7 +45,7 @@ sub parse {
 		'4932'  => 'Saccharomyces cerevisiae',
 		'4896'  => 'Schizosaccharomyces pombe'
 	  );
-
+my %hash_error; #hash of error, retrieve of uniprot or gene name from internet;
 	my %hash_uniprot_id; # A hash to store the uniprot id corresponding to a gene name and an organism
 	      # This avoid to run the same request several times in the uniprot.org server
 	if (-f "gene_name_to_uniprot_database.txt")   {
@@ -63,7 +63,7 @@ sub parse {
 
 	my $i = 0;
 
-	open( gene_name_to_uniprot_file, "gene_name_to_uniprot_database.txt" )
+	open( gene_name_to_uniprot_file, ">>gene_name_to_uniprot_database.txt" )
 	  ; # During this time, we complete the file which contains the uniprot id for a gene name and an organism
 	while (<data_file>) {
 		print "\n---------------------------------------------------\n" if ($main::verbose);		
@@ -110,7 +110,7 @@ sub parse {
 		
 		$uniprot_B = $1 if ($data[1] =~ /.+\|uniprotkb:(.+)$/);
 		next if (!$uniprot_B);
-		print "[DEBUG : DIP] uniprot A : $uniprot_A\n" if ($main::verbose);
+		print "[DEBUG : DIP] uniprot B : $uniprot_A\n" if ($main::verbose);
 		
 		
 		if ( exists( $hash_uniprot_id{$uniprot_A} ) )
